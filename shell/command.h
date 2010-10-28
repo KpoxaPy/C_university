33fd19e7484a32b3f32d3a56b3a4b684a6490efc
Andrew Ozmitel <kpoxapy@gmail.com>
2010-10-28 09:43:10 +0400

    I've started to describe command tree.

--- shell/command.h
+++ shell/command.h
@@ -4,6 +4,11 @@
 #include "main.h"
 #include "words.h"
 
+#define CMD_ACTION_NEXT 0
+#define CMD_ACTION_BG 1
+#define CMD_ACTION_AND_CHAIN 2
+#define CMD_ACTION_OR_CHAIN 3
+
 struct command {
 	char * file;
 	int bg;
@@ -12,8 +17,26 @@ struct command {
 	struct wordlist * words;
 };
 
-struct command * genCommand(struct wordlist *);
+struct cmdElem {
+	struct cmdElem * firstChild;
+	struct cmdElemAction * next;
+	struct command * cmd;
+};
+
+struct cmdElemAction {
+	int cmdAction;
+	struct cmdElem * second;
+	struct cmdElem * fir
+
+
+/* Command actions */
+stuct command * genCommand(struct wordlist *);
 void delCommand(struct command ** );
 void echoCommand(struct command *);
 
-#endif
+/* Cmd tree actions */
+struct cmdElem * createCmdElem(struct command *, struct cmdElem *);
+void removeCmdElem(struct cmdElem *);
+
+struct cmdElemAction * createCmdElemAction(int,	struct cmdElem *, struct cmdElem *);
+void removeCmdElemAction(struct cmdElemAction *);
